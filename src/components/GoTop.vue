559fa3ed0c1afdaf2c46c02439fa6c91e2f43ca5
duanlei <1969563716@qq.com>
2018-09-12 23:47:53 +0800

    addfile

--- src/components/GoTop.vue
+++ src/components/GoTop.vue
@@ -1,46 +1,51 @@
 <template>
   <div id="gotop">
-    <div class="btn" v-show = "show">回到顶部</div>
+    <div class="btn" v-show = "show" @click="goTop">回到顶部</div>
   </div>
 </template>
 <script>
 export default {
-  name: 'GoTop',
-  data: function (){
+  name: "GoTop",
+  data: function() {
     return {
       show: false
-    }
+    };
   },
-  mounted: function() {
-    window.onScroll = function () {
-      if(window.scrollTop() > 50) {
-        this.show = true
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    goTop() {
+      window.scrollTo(0, 0);
+      this.show = false;
+    },
+    handleScroll() {
+      if (window.pageYOffset > 150) {
+        this.show = true;
       } else {
         this.show = false;
       }
     }
-  },
-  methods: {
-    goTop() {
-      window.scrollTop(0);
-      this.show = false;
-    }
   }
-}
+};
 </script>
 <style lang="scss" scoped>
 #gotop .btn {
   font-size: 16px;
   width: 30px;
   border-radius: 3px;
-  padding: 12px 6px;;
+  padding: 12px 6px;
   text-align: center;
   color: grey;
   background: #fff;
   position: fixed;
   right: 10px;
   bottom: 100px;
-} 
+  cursor: pointer;
+}
 </style>
 
 
